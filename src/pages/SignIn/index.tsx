6ab2fd957b399f84e5b15a1db2f7e152a4e6bf01
elnto de campos <eltoncampos36@gmail.com>
2021-02-03 00:51:05 -0300

    [Feat] - create button and input components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,12 +4,19 @@ import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 import { Image } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
